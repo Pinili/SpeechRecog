f9d859db8ae87ca20a2928ba1463b68039cde2ce
Nerman <n.nicholas110@gmail.com>
2013-03-15 13:00:11 -0400

    ...

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -1,5 +1,7 @@
 package MainPackage;
-
+//20% complete!
 public class Main {
-
+	public static void main(String[] args){
+		
+	}
 }
--- src/MainPackage/VAnalyzer.java
+++ src/MainPackage/VAnalyzer.java
@@ -2,7 +2,6 @@ package MainPackage;
 
 // 7 days left till completion
 //YOLO
-
 public class VAnalyzer {
-
+	
 }
